471fe136e427d42dd5792f328acc72d77dc3d482
AbinMuds <abinashmudbhari@gmail.com>
2022-04-20 02:16:46 -0600

    added Error handling to unavailable routes for post or which uses bodyparser

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const HttpError = require("./models/http-error");
+
 const placesRoutes = require("./routes/places-routes"); //this is our middleware
 const usersRoutes = require("./routes/users-routes");
 
@@ -11,6 +13,11 @@ app.use(bodyParser.json());
 app.use("/api/places", placesRoutes); // => app/places/...
 
 app.use("/api/users", usersRoutes);
+
+app.use((req, res, next) => {
+  const error = new HttpError("Could not find this route", 404);
+  throw error;
+});
 
 app.use((error, req, res, next) => {
   if (res.headerSent) {
