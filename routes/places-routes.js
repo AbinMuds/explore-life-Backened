0ea1826e53859f6851d63cffb6ace884f1df0410
AbinMuds <abinashmudbhari@gmail.com>
2022-04-20 01:45:18 -0600

    adds all functionality in controllers and adds only routes functionality in routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,64 +1,13 @@
 const express = require("express");
 
-const HttpError = require("../models/http-error");
-
 const router = express.Router();
 
-const DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/The_Empire_State_Building_%2812%29.jpg/449px-The_Empire_State_Building_%2812%29.jpg?20200421163444",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.748817,
-      lng: -73.985428,
-    },
-    creator: "u1",
-  },
-  {
-    id: "p2",
-    title: "Emp. State Building",
-    description: "One of the most famous sky scrapers in the world",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/The_Empire_State_Building_%2812%29.jpg/449px-The_Empire_State_Building_%2812%29.jpg?20200421163444",
-    address: "20 W 34th St, New York, NY 10001",
-    location: {
-      lat: 40.748817,
-      lng: -73.985428,
-    },
-    creator: "u2",
-  },
-];
+const placesControllers = require("../controllers/places-controller");
 
-router.get("/:pid", (req, res, next) => {
-  const placeId = req.params.pid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.id === placeId;
-  });
+router.get("/:pid", placesControllers.getPlaceById);
 
-  if (!place) {
-    throw new HttpError(
-      "Could not find a place for the provided place id",
-      404
-    );
-  }
-  res.json({ place });
-});
+router.get("/user/:uid", placesControllers.getPlaceByUserId);
 
-router.get("/user/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const place = DUMMY_PLACES.find((p) => {
-    return p.creator === userId;
-  });
-  if (!place) {
-    return next(
-      new HttpError("Could not find a place for the provided user id", 404)
-    );
-  }
-  res.json({ place });
-});
+router.post("/", placesControllers.createPlace);
 
 module.exports = router;
