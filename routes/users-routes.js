874517d37ca59bf0a994443c6f7bd9acc5a81116
AbinMuds <abinashmudbhari@gmail.com>
2022-04-20 21:08:06 -0600

    implemented express-validator in the routes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,23 +1,21 @@
 const express = require("express");
-
+const { check } = require("express-validator");
 const router = express.Router();
 
-const USERS = [
-  {
-    id: "u1",
-    name: "Avi",
-    image:
-      "https://media-exp1.licdn.com/dms/image/C4E03AQHzQDXXa6pJXA/profile-displayphoto-shrink_200_200/0/1613667850675?e=1652313600&v=beta&t=NenQfI_iT9M5EQ76_NRXo-C3Kr-blhtgvTtt6M1UXBU",
-    places: 3,
-  },
-];
+const usersController = require("../controllers/users-controller");
 
-router.get("/:uid", (req, res, next) => {
-  const userId = req.params.uid;
-  const user = USERS.find((u) => {
-    return u.id === userId;
-  });
-  res.json({ user });
-});
+router.get("/", usersController.getUsers);
+
+router.post(
+  "/signUp",
+  [
+    check("name").not().isEmpty(),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 6 }),
+  ],
+  usersController.signUp
+);
+
+router.post("/login", usersController.login);
 
 module.exports = router;
